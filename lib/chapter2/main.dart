54d671e313adc395811312320dab40c561b6d9d3
hide0621 <fujihara_hideyuki@retail-ai.jp>
2024-02-29 09:40:52 +0900

    MaterialAppにScaffoldを当て込み、そこにappBar(アプリケーションバー)とbodyに紐づく各種Widgetを当てこんでみた

--- lib/chapter2/main.dart
+++ lib/chapter2/main.dart
@@ -10,11 +10,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      home: Text(
-        'Hello, Flutter Demo',
-        style: TextStyle(fontSize: 32),
-      ),
-    );
+        title: 'Flutter Demo',
+        home: Scaffold(
+          appBar: AppBar(
+            title: const Text('Hello Flutter!'),
+          ),
+          body: const Text(
+            'Hello, Flutter Demo',
+            style: TextStyle(fontSize: 32),
+          ),
+        ));
   }
 }
