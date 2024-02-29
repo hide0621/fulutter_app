1d3521cc05603e65e8d0ab0df7335b4cab27ba2b
hide0621 <fujihara_hideyuki@retail-ai.jp>
2024-02-29 14:24:52 +0900

    StatelessWidgetとStateを使い動的に表示が変わるようにしてみた

--- lib/chapter2/main.dart
+++ lib/chapter2/main.dart
@@ -5,20 +5,44 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
+  final title = 'Flutter サンプル';
+  final message = 'サンプル・メッセージ';
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter Demo',
-        home: Scaffold(
-          appBar: AppBar(
-            title: const Text('Hello Flutter!'),
-          ),
-          body: const Text(
-            'Hello, Flutter Demo',
-            style: TextStyle(fontSize: 32),
-          ),
+      title: 'Flutter Demo',
+      home: MyHomePage(
+        title: this.title,
+        message: this.message,
+      ),
+    );
+  }
+}
+
+// 動的に表示が切り替わるように、このApp専用のWidgetをWrapして作成
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title, required this.message})
+      : super(key: key);
+  final String title;
+  final String message;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+// 上記で作成したWidgetに紐づくこのApp専用のStateを作成
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: Text(
+          widget.message,
+          style: TextStyle(fontSize: 32.0),
         ));
   }
 }
